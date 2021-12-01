28b154e9b9f7a22b641a00ff6b70b183432cea76
ozansulukpinar <ozansulukpinar@hotmail.com>
2021-12-02 00:24:57 +0300

    Commit #1 - Files added, readme updated

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Calculator
+
+A simple calculator using HTML, CSS and jQuery
+
+## Table of contents
+
+- [General info](#general-info)
+- [Technologies](#technologies)
+- [Status](#status)
+- [Contact](#contact)
+
+## General info
+
+If you check some profile of developers in GitHub randomly, there is a very high probability of seeing same project in their profile. The name of this project: Calculator. As a necessity of being a GitHub user, a calculator that is performed the four arithmetic operations of addition, subtraction, multiplication, and division was created.
+
+## Technologies
+
+Project is created with:
+
+- Javascript
+
+## Status
+
+Project is finished.
+
+## Contact
+
+Created by [@ozansulukpinar](https://github.com/ozansulukpinar) - feel free to contact me!
---
+++ app.js
@@ -0,0 +1,141 @@
+var calculation = "";
+var signs = ["+", "-", "*", "/"];
+
+$("#main").on("click", ".box", function () {
+  var id = jQuery(this).closest("th").attr("id");
+
+  if (id === "=") {
+    if (
+      calculation === "" ||
+      (calculation.indexOf("+") < 0 &&
+        calculation.indexOf("-") < 0 &&
+        calculation.indexOf("*") < 0 &&
+        calculation.indexOf("/") < 0)
+    ) {
+      // do nothing
+    } else {
+    // make calculation
+            var indexes = [
+        calculation.indexOf("+"),
+        calculation.indexOf("-"),
+        calculation.indexOf("*"),
+        calculation.indexOf("/")
+      ];
+      
+            var temp = 0;
+      var tempIndex = 0;
+
+      for (var index in indexes) {
+        if (indexes[index] != -1) {
+          temp = signs[index];          
+          tempIndex = index;
+        }
+      }
+
+      var firstNumber = calculation.substring(0,
+        calculation.indexOf(temp));
+      var secondNumber = calculation.substring(
+        calculation.indexOf(temp) + 1
+      );     
+      
+      var result;
+      
+      switch(tempIndex){
+        case "0":
+      result = Number(firstNumber) + Number(secondNumber);          
+          break;
+        case "1":
+          result = Number(firstNumber) - Number(secondNumber);          
+          break;
+        case "2":
+          result = Number(firstNumber) * Number(secondNumber);          
+          break;
+        case "3":
+          result = Number(firstNumber) / Number(secondNumber);          
+          break;
+        default:
+          // do nothing
+      }
+      
+      $("#result").text(result);
+      
+      result = result + "";
+      if(result  === "Infinity" || result === "NaN"){        
+        calculation = "";
+      }
+      else{
+        calculation = result;   
+      }
+    }
+  } else if (id === "C") {
+    // delete current calculation
+    calculation = "";
+    $("#result").text("");
+  } else if (id === "+" || id === "-" || id === "*" || id === "/") {
+    if (
+      calculation.indexOf("+") >= 0 ||
+      calculation.indexOf("-") >= 0 ||
+      calculation.indexOf("*") >= 0 ||
+      calculation.indexOf("/") >= 0
+    ) {
+      // do nothing
+    } else {
+      if (calculation === "") {
+        calculation = "0";
+      }
+      calculation = calculation + id;
+      $("#result").text(calculation);
+    }
+  } else if (id === ".") {    
+    if (calculation.indexOf(".") < 0) {
+      // there is no point
+      if (calculation === "") {
+        // do nothing
+      } else {
+        calculation = calculation + id;
+        $("#result").text(calculation);
+      }
+    } else if (
+      calculation.indexOf("+") >= 0 ||
+      calculation.indexOf("-") >= 0 ||
+      calculation.indexOf("*") >= 0 ||
+      calculation.indexOf("/") >= 0
+    ) {
+      var indexes = [
+        calculation.indexOf("+"),
+        calculation.indexOf("-"),
+        calculation.indexOf("*"),
+        calculation.indexOf("/")
+      ];
+
+      var temp = 0;
+
+      for (var index in indexes) {
+        if (indexes[index] != -1) {
+          temp = signs[index];
+        }
+      }
+
+      var temporaryCalculation = calculation.substring(
+        calculation.indexOf(temp) + 1
+      );
+      if (temporaryCalculation.indexOf(".") < 0) {
+        calculation = calculation + id;
+        $("#result").text(calculation);
+      } else {
+        // do nothing
+      }
+    } else if (
+      calculation.indexOf(".") >= 0 &&
+      (calculation.indexOf("+") < 0 ||
+        calculation.indexOf("-") < 0 ||
+        calculation.indexOf("*") < 0 ||
+        calculation.indexOf("/") < 0)
+    ) {
+      // do nothing
+    }
+  } else {
+    calculation = calculation + id;
+    $("#result").text(calculation);
+  }
+});
